2691abacd698da7728cb1998154bdb7dd43723c1
Rishany2004 <159290227+Rishany2004@users.noreply.github.com>
2024-05-11 22:39:16 +0530

    Update Exp4_SRflipflop.v

--- Exp4_SRflipflop.v
+++ Exp4_SRflipflop.v
@@ -1,12 +1,23 @@
-module sr( s,r,clk,q);
- input s,r,clk; 
-output reg q=0; 
-
-
-
-
-endmodule
-
+module SRflipflop(s,r,clk,rst,q);
+input s,r,clk,rst;
+output q;
+reg q;
+always@(posedge clk)
+begin
+ if(rst)
+ q<=1'b0;
+ else
+  begin
+    case({s,r})
+     2'b00:q<=q;
+     2'b01:q<=1'b0;
+     2'b10:q<=1'b1;
+     2'b11:q<=1'bx;
+    default:q<=1'bx;
+    endcase
+    end
+   end
+ endmodule
 
 
 
